d09007da2401d3b04945bd153615d281ea6f9755
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-12 12:19:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+function numberOfBooksRead(library) {
+  // Check if the library object is provided and has a books property
+  if (!library || !library.books || !Array.isArray(library.books)) {
+    console.error("Invalid library object");
+    return 0;
+  }
 
-const numberOfBooksRead = () => {
-  // write your code here
+  // Filter the array to get only the books that have been read
+  const readBooks = library.books.filter(book => book.readingStatus === true);
+
+  // Return the count of read books
+  return readBooks.length;
+}
+
+// Example usage:
+const library = {
+  books: [
+    { author: "Author1", title: "Book1", readingStatus: true },
+    { author: "Author2", title: "Book2", readingStatus: false },
+    { author: "Author3", title: "Book3", readingStatus: true },
+    // ... other books
+  ],
 };
 
-// Do not change the code below
+const result = numberOfBooksRead(library);
+console.log(result); // Output: 2
 
-alert(numberOfBooksRead());
